3e7edf7c47015f405f445c6fc43f50900801fe83
Nolan J Tait <nolanjtait@gmail.com>
2022-02-07 16:28:18 -0800

    Initial commit

---
+++ spec/bankroll_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Bankroll do
+  it "has a version number" do
+    expect(Bankroll::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
